075aa52aa8aa0da1681e85fca87d1e9d412bc01c
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-08-23 15:15:14 +0200

    model crud seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'email/index'
+  get 'emails', to: "emails#index"
+  get "emails/:id", to: "emails#show"
+  delete "emails/:id", to: "emails#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "email#index"
 end
